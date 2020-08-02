12369335dcedada769d659824749d4f503801a10
xieguigang <xie.guigang@gcmodeller.org>
2020-08-02 21:21:58 +0800

    algorithm folder

---
+++ PlantMAT.Core/PlantMAT.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' PlantMAT: A Metabolomics Tool for Predicting the Specialized 
+''' Metabolic Potential of a System and for Large-Scale Metabolite 
+''' Identifications
+''' </summary>
+<Package("PlantMAT", Category:=APICategories.ResearchTools, Cites:="", Url:="https://pubs.acs.org/doi/10.1021/acs.analchem.6b00906")>
+Module PlantMAT
+
+End Module
