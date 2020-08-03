db9ab0059e6ab34b40465c4fd193c7af89d7b56e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-03 10:32:42 +0800

    new projects module

---
+++ Core/Models/Library.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
+
+Public Class Library
+
+    <Column("Common Name")>
+    Public Property CommonName As String
+    Public Property [Class] As String
+    Public Property Type As String
+    Public Property Formula As String
+
+    <Column("Exact Mass")>
+    Public Property ExactMass As Double
+    Public Property Genus As String
+
+    <Column("Universal SMILES")>
+    Public Property Universal_SMILES As String
+    Public Property Editor As String
+    Public Property [Date] As Date
+
+    Public Overrides Function ToString() As String
+        Return CommonName
+    End Function
+
+End Class
---
+++ Core/Models/Query.vb
@@ -0,0 +1,22 @@
+﻿Public Class Query
+
+    Public Property PeakNO As String
+    Public Property PrecursorIon As Double
+
+    Public Shared Function ParseMs1PeakList(file As IEnumerable(Of String)) As Query
+
+    End Function
+
+End Class
+
+Public Class CandidateResult
+
+    Public Property SubstructureAgly As String
+    Public Property Hex As String
+    Public Property HexA As String
+    Public Property dHex As String
+    Public Property Pen As String
+    Public Property Mal As String
+    Public Property Err As Double
+
+End Class
