138469986cfb6544dd5ddeb2477b0930209b6305
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-03 14:46:39 +0800

    do ms1 search

--- Core/Algorithm/MS1TopDown.vb
+++ Core/Algorithm/MS1TopDown.vb
@@ -1,4 +1,7 @@
-﻿''' <summary>
+﻿Imports Microsoft.VisualBasic.Linq
+Imports WorksheetFunction = Microsoft.VisualBasic.Math.VBMath
+
+''' <summary>
 ''' This module performs combinatorial enumeration
 ''' </summary>
 Module MS1TopDown
@@ -32,6 +35,7 @@ Module MS1TopDown
 
     Dim Pattern_n As Integer
     Dim library As Library()
+    Dim settings As Settings
 
     Sub MS1CP(query As IEnumerable(Of Query))
 
@@ -68,7 +72,7 @@ Module MS1TopDown
 
         'Run combinatorial enumeration
         Pattern_n = 0
-        Call MS1_CombinatorialPrediction(query)
+        Call MS1_CombinatorialPrediction(query, settings.PrecursorIonMZ, settings.PrecursorIonN)
 
         'Show columns of sugar/acid if any >=1
         'For j = 8 To 16
@@ -102,8 +106,14 @@ Module MS1TopDown
 
     Dim NumHexMin, NumHexMax, NumHexAMin, NumHexAMax, NumdHexMin, NumdHexMax, NumPenMin, NumPenMax, NumMalMin, NumMalMax, NumCouMin, NumCouMax, NumFerMin, NumFerMax, NumSinMin, NumSinMax, NumDDMPMin, NumDDMPMax As Integer
 
+    ''' <summary>
+    ''' search for given precursor_type
+    ''' </summary>
+    ''' <param name="queries"></param>
+    ''' <param name="PrecursorIonMZ">adducts</param>
+    ''' <param name="PrecursorIonN">M</param>
     Sub MS1_CombinatorialPrediction(queries As IEnumerable(Of Query), PrecursorIonMZ As Double, PrecursorIonN As Integer)
-        Dim AllSMILES As String
+        '   Dim AllSMILES As String
 
         Pattern_n = 0
 
@@ -114,9 +124,9 @@ Module MS1TopDown
             Dim ErrorCheck = False
             Dim RT_E = query.PeakNO
             Dim M_w = (query.PrecursorIon - PrecursorIonMZ) / PrecursorIonN
-            i_prev = i
-            AllSMILES = ""
-            Candidate_n = 0
+            ' i_prev = i
+            ' AllSMILES = ""
+            ' Candidate_n = 0
             Dim Candidate As New List(Of CandidateResult)
 
             If M_w <= 0 Or M_w > 2000 Then
@@ -133,7 +143,7 @@ Module MS1TopDown
                                         For Sin_n = NumSinMin To NumSinMax
                                             For DDMP_n = NumDDMPMin To NumDDMPMax
 
-                                                Call MS1_CombinatorialPrediction_RestrictionCheck(Hex_n, HexA_n, dHex_n, Pen_n, Mal_n, Cou_n, Fer_n, Sin_n, DDMP_n, M_w)
+                                                Call MS1_CombinatorialPrediction_RestrictionCheck(RT_E, Hex_n, HexA_n, dHex_n, Pen_n, Mal_n, Cou_n, Fer_n, Sin_n, DDMP_n, M_w).DoCall(AddressOf Candidate.AddRange)
 
                                             Next DDMP_n
                                         Next Sin_n
@@ -145,12 +155,11 @@ Module MS1TopDown
                 Next HexA_n
             Next Hex_n
 
-            'ResultDisplay:
-            '            Call MS1_CombinatorialPrediciton_ResultDisplay(query)
+            query.Candidates = Candidate
 
-            ' i = i + 1
+ResultDisplay:
+            Call MS1_CombinatorialPrediciton_PatternPrediction(query)
         Next
-
     End Sub
 
     Dim NumSugarMin, NumSugarMax, NumAcidMin, NumAcidMax As Integer
@@ -172,7 +181,7 @@ Module MS1TopDown
         H_w = 1.00782504,
         e_w = 0.00054858
 
-    Sub MS1_CombinatorialPrediction_RestrictionCheck(Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%, M_w As Double)
+    Iterator Function MS1_CombinatorialPrediction_RestrictionCheck(RT_E#, Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%, M_w As Double) As IEnumerable(Of CandidateResult)
 
         Dim Sugar_n = Hex_n + HexA_n + dHex_n + Pen_n
         Dim Acid_n = Mal_n + Cou_n + Fer_n + Sin_n + DDMP_n
@@ -185,16 +194,18 @@ Module MS1TopDown
             Dim nH2O_w = (Sugar_n + Acid_n) * H2O_w
             Dim Bal = M_w + nH2O_w - Attn_w
 
-            If Bal >= AglyconeMWLL And Bal <= AglyconeMWUL Then
-                Call MS1_CombinatorialPrediciton_InternalDatabase(M_w#, Attn_w#, nH2O_w#)
-
+            ' "Aglycone MW Range" Then AglyconeMWLL = minValue : AglyconeMWUL = maxValue
+            If Bal >= settings.AglyconeMWRange(0) And Bal <= settings.AglyconeMWRange(1) Then
+                For Each candidate In MS1_CombinatorialPrediciton_InternalDatabase(RT_E:=RT_E, M_w#, Attn_w#, nH2O_w#, Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%)
+                    Yield candidate
+                Next
             End If
 
         End If
 
-    End Sub
+    End Function
 
-    Sub MS1_CombinatorialPrediciton_InternalDatabase(M_w#, Attn_w#, nH2O_w#)
+    Iterator Function MS1_CombinatorialPrediciton_InternalDatabase(RT_E#, M_w#, Attn_w#, nH2O_w#, Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%) As IEnumerable(Of CandidateResult)
 
         '  LastRow = Database.Range("B" & Rows.Count).End(xlUp).Row
 
@@ -207,15 +218,15 @@ Module MS1TopDown
             'AglyW = Database.Cells(j, 6)
             'AglyS = Database.Cells(j, 8)
 
-            Call MS1_CombinatorialPrediciton_DatabaseSearch(AglyN:=ref.CommonName,
+            For Each candidate In MS1_CombinatorialPrediciton_DatabaseSearch(RT_E:=RT_E, AglyN:=ref.CommonName,
             AglyT:=ref.Class,
             AglyO:=ref.Genus,
             AglyW:=ref.ExactMass,
-            AglyS:=ref.Universal_SMILES, M_w:=M_w, Attn_w:=Attn_w, nH2O_w:=nH2O_w)
-
+            AglyS:=ref.Universal_SMILES, M_w:=M_w, Attn_w:=Attn_w, nH2O_w:=nH2O_w, Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%)
+                Yield candidate
+            Next
         Next
-
-    End Sub
+    End Function
 
     'Sub MS1_CombinatorialPrediciton_ExternalDatabase(ExternalAglyconeDatabase As String)
 
@@ -234,7 +245,7 @@ Module MS1TopDown
 
     'End Sub
 
-    Sub MS1_CombinatorialPrediciton_DatabaseSearch(AglyN$, AglyT$, AglyO$, AglyW#, AglyS$, M_w#, Attn_w#, nH2O_w#)
+    Iterator Function MS1_CombinatorialPrediciton_DatabaseSearch(RT_E#, AglyN$, AglyT$, AglyO$, AglyW#, AglyS$, M_w#, Attn_w#, nH2O_w#, Hex_n%, HexA_n%, dHex_n%, Pen_n%, Mal_n%, Cou_n%, Fer_n%, Sin_n%, DDMP_n%) As IEnumerable(Of CandidateResult)
 
         If AglyT = AglyconeType.ToString Or AglyconeType = db_AglyconeType.All Then
             If AglyO = AglyconeSource.ToString Or AglyconeSource = db_AglyconeSource.All Then
@@ -242,102 +253,128 @@ Module MS1TopDown
                 Dim Err1 = Math.Abs((M_w - (AglyW + Attn_w - nH2O_w)) / (AglyW + Attn_w - nH2O_w)) * 1000000
 
                 If Err1 <= SearchPPM Then
-                    RT_P = 0
+                    Dim RT_P = 0
 
-                    Candidate_n = Candidate_n + 1
-                    ReDim Preserve Candidate(0 To 14, 1 To Candidate_n)
-                    Candidate(0, Candidate_n) = AglyW
-                    Candidate(1, Candidate_n) = AglyS
-                    Candidate(2, Candidate_n) = AglyN
-                    Candidate(3, Candidate_n) = Hex_n
-                    Candidate(4, Candidate_n) = HexA_n
-                    Candidate(5, Candidate_n) = dHex_n
-                    Candidate(6, Candidate_n) = Pen_n
-                    Candidate(7, Candidate_n) = Mal_n
-                    Candidate(8, Candidate_n) = Cou_n
-                    Candidate(9, Candidate_n) = Fer_n
-                    Candidate(10, Candidate_n) = Sin_n
-                    Candidate(11, Candidate_n) = DDMP_n
-                    Candidate(12, Candidate_n) = Err1
-                    Candidate(13, Candidate_n) = RT_P
-                    Candidate(14, Candidate_n) = CStr(RT_P - RT_E)
+                    '  Candidate_n = Candidate_n + 1
+                    '   ReDim Preserve Candidate(0 To 14, 1 To Candidate_n)
+                    Yield New CandidateResult With {
+                        .ExactMass = AglyW,
+                        .Name = AglyN,
+                        .Hex = Hex_n,
+                        .HexA = HexA_n,
+                        .dHex = dHex_n,
+                        .Pen = Pen_n,
+                        .Mal = Mal_n,
+                        .Err = Err1,
+                        .SubstructureAgly = AglyS,
+                        .Cou = Cou_n,
+                        .DDMP = DDMP_n,
+                        .Fer = Fer_n,
+                        .Sin = Sin_n,
+                        .RT = RT_P,
+                        .RTErr = RT_P - RT_E
+                    }
+
+                    ' Candidate(0, Candidate_n) =
+                    ' Candidate(1, Candidate_n) = AglyS
+                    ' Candidate(2, Candidate_n) = AglyN
+                    'Candidate(3, Candidate_n) = Hex_n
+                    'Candidate(4, Candidate_n) = HexA_n
+                    'Candidate(5, Candidate_n) = dHex_n
+                    'Candidate(6, Candidate_n) = Pen_n
+                    'Candidate(7, Candidate_n) = Mal_n
+                    'Candidate(8, Candidate_n) = Cou_n
+                    'Candidate(9, Candidate_n) = Fer_n
+                    'Candidate(10, Candidate_n) = Sin_n
+                    'Candidate(11, Candidate_n) = DDMP_n
+                    'Candidate(12, Candidate_n) = Err1
+                    'Candidate(13, Candidate_n) = RT_P
+                    'Candidate(14, Candidate_n) = CStr(RT_P - RT_E)
                 End If
 
             End If
         End If
 
+    End Function
+
+    'Sub MS1_CombinatorialPrediciton_ResultDisplay(query As Query)
+
+    '    If Candidate_n = 0 Then
+    '        query.no_hit = True
+    '    Else
+    '        For m = 1 To Candidate_n
+    '            '   DoEvents
+    '            Dim max_temp = 100
+    '            For n = 1 To Candidate_n
+    '                '  DoEvents
+    '                If Right(Candidate(14, n), 1) <> "*" And Math.Abs(Val(Candidate(14, n))) < max_temp Then
+    '                    max_temp = Math.Abs(Val(Candidate(14, n)))
+    '                    k = n
+    '                End If
+    '            Next n
+
+    '            With PublicVS_Code.Query
+    '                If m > 1 Then
+    '                    ' Call .Cells(i, 4).Offset(1).EntireRow.Insert
+    '                    i = i + 1
+    '                    '  .Cells(i, 4) = "..."
+    '                End If
+    '                query.comment = CStr(Candidate(0, k))
+    '                ' .Cells(i, 7).Comment.Shape.TextFrame.AutoSize = True
+    '                For q = 2 To 12
+    '                    ' .Cells(i, q + 5) = Candidate(q, k)
+    '                    query.candidates.Add(Candidate(q, k))
+    '                Next q
+    '                '  .Range(Cells(i, 7), Cells(i, 20)).Font.Color = RGB(0, 0, 0)
+    '                '.Cells(i, 7).HorizontalAlignment = xlLeft
+    '                If max_temp <> RT_E Then
+    '                    ' .Cells(i, 19) = Candidate(13, k)
+    '                    '  .Cells(i, 20) = Candidate(14, k)
+    '                    Dim RT_Diff = Math.Abs(Val(Candidate(14, k)))
+    '                    ' If RT_Diff <= 0.5 Then .Cells(i, 20).Font.Color = RGB(118, 147, 60)
+    '                    ' If RT_Diff > 0.5 And RT_Diff <= 1 Then .Cells(i, 20).Font.Color = RGB(255, 192, 0)
+    '                    ' If RT_Diff > 1 Then .Cells(i, 20).Font.Color = RGB(192, 80, 77)
+    '                Else
+    '                    '  If RetentionPrediction = True Then
+    '                    '   .Range(Cells(i, 19), Cells(i, 20)) = "n/a"
+    '                    '  .Range(Cells(i, 19), Cells(i, 20)).Font.Color = RGB(217, 217, 217)
+    '                    'End If
+    '                End If
+    '            End With
+
+    '            Candidate(14, k) = Candidate(14, k) + "*"
+
+    '            If PatternPrediction = True Then Call MS1_CombinatorialPrediciton_PatternPrediction() 'Pattern Prediction
+    '        Next m
+    '    End If
+
+    'End Sub
+
+    Sub MS1_CombinatorialPrediciton_PatternPrediction(query As Query)
+        For m As Integer = 0 To query.Candidates.Count - 1
+            Dim candidate As CandidateResult = query(m)
+
+            Call MS1_CombinatorialPrediciton_PatternPrediction(candidate, m)
+        Next
     End Sub
 
-    Sub MS1_CombinatorialPrediciton_ResultDisplay(query As Query)
-
-        If Candidate_n = 0 Then
-            query.no_hit = True
-        Else
-            For m = 1 To Candidate_n
-                '   DoEvents
-                Dim max_temp = 100
-                For n = 1 To Candidate_n
-                    '  DoEvents
-                    If Right(Candidate(14, n), 1) <> "*" And Math.Abs(Val(Candidate(14, n))) < max_temp Then
-                        max_temp = Math.Abs(Val(Candidate(14, n)))
-                        k = n
-                    End If
-                Next n
-
-                With PublicVS_Code.Query
-                    If m > 1 Then
-                        ' Call .Cells(i, 4).Offset(1).EntireRow.Insert
-                        i = i + 1
-                        '  .Cells(i, 4) = "..."
-                    End If
-                    query.comment = CStr(Candidate(0, k))
-                    ' .Cells(i, 7).Comment.Shape.TextFrame.AutoSize = True
-                    For q = 2 To 12
-                        ' .Cells(i, q + 5) = Candidate(q, k)
-                        query.candidates.Add(Candidate(q, k))
-                    Next q
-                    '  .Range(Cells(i, 7), Cells(i, 20)).Font.Color = RGB(0, 0, 0)
-                    '.Cells(i, 7).HorizontalAlignment = xlLeft
-                    If max_temp <> RT_E Then
-                        ' .Cells(i, 19) = Candidate(13, k)
-                        '  .Cells(i, 20) = Candidate(14, k)
-                        Dim RT_Diff = Math.Abs(Val(Candidate(14, k)))
-                        ' If RT_Diff <= 0.5 Then .Cells(i, 20).Font.Color = RGB(118, 147, 60)
-                        ' If RT_Diff > 0.5 And RT_Diff <= 1 Then .Cells(i, 20).Font.Color = RGB(255, 192, 0)
-                        ' If RT_Diff > 1 Then .Cells(i, 20).Font.Color = RGB(192, 80, 77)
-                    Else
-                        '  If RetentionPrediction = True Then
-                        '   .Range(Cells(i, 19), Cells(i, 20)) = "n/a"
-                        '  .Range(Cells(i, 19), Cells(i, 20)).Font.Color = RGB(217, 217, 217)
-                        'End If
-                    End If
-                End With
-
-                Candidate(14, k) = Candidate(14, k) + "*"
-
-                If PatternPrediction = True Then Call MS1_CombinatorialPrediciton_PatternPrediction() 'Pattern Prediction
-            Next m
-        End If
-
-    End Sub
-
-    Sub MS1_CombinatorialPrediciton_PatternPrediction()
+    Sub MS1_CombinatorialPrediciton_PatternPrediction(candidate As CandidateResult, m As Integer)
 
         '1. Find location and number of OH groups in aglycone
         Dim AglyS1 As String, AglyS2 As String
         Dim Hex As String, HexA As String, dHex As String, Pen As String
         Dim Cou As String, Fer As String, Sin As String, Mal As String
-        Dim e As Long, OH_n As Long
+        Dim OH_n As Long
         Dim n1 As Long, n2 As Long
 
-        AglyN = Candidate(2, k)
-        AglyS1 = Candidate(1, k)
+        Dim AglyN = candidate.Name
+        AglyS1 = candidate.SubstructureAgly
         AglyS2 = Strings.Replace(AglyS1, "O)", ".)")
         AglyS2 = Strings.Replace(AglyS2, "=.", "=O")
         If Right(AglyS2, 1) = "O" Then AglyS2 = Left(AglyS2, Len(AglyS2) - 1) & "."
 
         OH_n = 0
-        For e = 1 To Len(AglyS2)
+        For e As Integer = 1 To Len(AglyS2)
             If Mid(AglyS2, e, 1) = "." Then OH_n = OH_n + 1
         Next e
 
@@ -355,15 +392,18 @@ Module MS1TopDown
 
         '2. Find type and number of sugars/acids
         Dim Sug_n As Long
-        Dim Sug, Sug_p(,) As String
+        Dim Sug As String
+        Dim Sug_p(,) As String
         Dim g As Long, h As Long, l As Long
 
-        For e = 3 To 11
-            Sug_n = Sug_n + Candidate(e, k)
-        Next e
+        'For e = 3 To 11
+        ' Sug_n = Sug_n + candidate(e, k)
+        ' Next e
+        Sug_n = candidate.GetSug_nStatic.Sum
+
         If Sug_n = 0 Then Exit Sub
 
-        ReDim Sug_p(1, 1 To Sug_n)
+        Sug_p = New String(0, 0 To Sug_n - 1) {}
         l = 1
 
         Hex = "C?C(C(C(C(CO)O?)O)O)O"
@@ -375,9 +415,13 @@ Module MS1TopDown
         Fer = "COc?cc(ccc?O)C=CC(=O)O"
         Sin = "COc?cc(C=CC(=O)O)cc(c?O)OC"
         Dim DDMP = "CC?=C(C(=O)CC(O)O?)O"
+        Dim candidateSug_nStatic = candidate.GetSug_nStatic.ToArray
 
-        For e = 3 To 11
-            g = Candidate(e, k)
+        For i As Integer = 3 To 11
+            Dim e As Integer = i - 3
+
+            g = candidateSug_nStatic(e)
+
             If g > 0 Then
                 If e = 3 Then Sug = Hex
                 If e = 4 Then Sug = HexA
@@ -389,31 +433,31 @@ Module MS1TopDown
                 If e = 10 Then Sug = Sin
                 If e = 11 Then Sug = DDMP
                 For h = 1 To g
-                    Sug_p(1, l) = Sug
+                    Sug_p(0, l) = Sug
                     l = l + 1
                 Next h
             End If
-        Next e
+        Next
 
         '3. Permutate sugars/acids without repetition
         Dim c As Long, r As Long, p As Long
-        Dim rng() As Long, temp As Long
+        Dim rng(,) As Long, temp As Long
         Dim temp1 As Long, y() As Long, d As Long
 
         p = WorksheetFunction.Permut(Sug_n, Sug_n)
 
         '3.1 Create array
-        ReDim rng(1 To p, 1 To Sug_n)
-
+        ' Dim rng As New List(Of Long)
+        rng = New Long(0 To p - 1, 0 To Sug_n - 1) {}
 
         '3.2 Create first row in array (1, 2, 3, ...)
-        For c = 1 To Sug_n
-            rng(1, c) = c
+        For c = 0 To Sug_n - 1
+            rng(0, c) = c
         Next c
-        For r = 2 To p
+        For r = 1 To p
 
             '3.3 Find the first smaller number rng(r-1,c-1)<rng(r-1,c)
-            For c = Sug_n To 1 Step -1
+            For c = Sug_n - 1 To 0 Step -1
                 If rng(r - 1, c - 1) < rng(r - 1, c) Then
                     temp = c - 1
                     Exit For
@@ -421,18 +465,18 @@ Module MS1TopDown
             Next c
 
             '3.4 Copy values from previous row
-            For c = Sug_n To 1 Step -1
+            For c = Sug_n - 1 To 0 Step -1
                 rng(r, c) = rng(r - 1, c)
             Next c
 
             '3.5 Find a larger number than rng(r-1,temp) as far to the right as possible
-            For c = Sug_n To 1 Step -1
+            For c = Sug_n - 1 To 0 Step -1
                 If rng(r - 1, c) > rng(r - 1, temp) Then
                     temp1 = rng(r - 1, temp)
                     rng(r, temp) = rng(r - 1, c)
                     rng(r, c) = temp1
                     ReDim y(Sug_n - temp)
-                    e = 0
+                    Dim e = 0
                     For d = temp + 1 To Sug_n
                         y(e) = rng(r, d)
                         e = e + 1
@@ -450,10 +494,11 @@ Module MS1TopDown
         '4 Combine sugars/acids
         Dim z As Long, q As Long, s As Long
         Dim v As Long, w As Long, n As Long
-        Dim x() As String, t() As String, u() As String
-        ReDim x(1 To Sug_n, 1 To Sug_n)
-        ReDim t(100000, 1 To OH_n)
-        ReDim u(100000, 1 To OH_n)
+        Dim x(,) As String, t(,) As String, u(,) As String
+
+        x = New String(0 To Sug_n - 1, 0 To Sug_n - 1) {}
+        t = New String(100000 - 1, 0 To OH_n - 1) {}
+        u = New String(100000 - 1, 0 To OH_n - 1) {}
 
         w = 1
 
@@ -571,11 +616,7 @@ AllSugarConnected:
 
             GlycN = AglyN + SugComb
 
-            With SMILES
-                .Cells(Pattern_n + 3, 2) = PublicVS_Code.Query.Cells(i_prev, 2)
-                .Cells(Pattern_n + 3, 3) = CStr(m) + "-" + CStr(e)
-                .Cells(Pattern_n + 3, 4) = GlycN
-            End With
+            candidate.SMILES.Add({CStr(m) + "-" + CStr(e), GlycN})
 
             Pattern_n = Pattern_n + 1
         Next e
