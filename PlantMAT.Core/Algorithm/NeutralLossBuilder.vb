01d904069261454cd399e1b6ff3200124018879b
xieguigang <xie.guigang@gcmodeller.org>
2021-05-12 17:41:38 +0800

    Update NeutralLossBuilder.vb

--- PlantMAT.Core/Algorithm/NeutralLossBuilder.vb
+++ PlantMAT.Core/Algorithm/NeutralLossBuilder.vb
@@ -6,15 +6,27 @@ Namespace Algorithm
     Public Class NeutralLossBuilder
 
         ReadOnly groups As New Dictionary(Of String, NeutralGroup)
+        ReadOnly strict As Boolean = False
 
-        Sub New(groups As IEnumerable(Of NeutralGroup))
+        Sub New(groups As IEnumerable(Of NeutralGroup), Optional strict As Boolean = False)
             Me.groups = groups.ToDictionary(Function(element) element.ionName)
+            Me.strict = strict
         End Sub
 
         Public Function CreateLoss(lossList As Dictionary(Of String, Integer)) As NeutralLoss
             Dim hits As New List(Of NeutralGroupHit)
 
             For Each lossElement As KeyValuePair(Of String, Integer) In lossList
+                If Not groups.ContainsKey(lossElement.Key) Then
+                    Call $"missing neutral element group: {lossElement.Key}!".Warning
+
+                    If strict Then
+                        Throw New KeyNotFoundException($"missing neutral element group: {lossElement.Key}!")
+                    Else
+                        Return Nothing
+                    End If
+                End If
+
                 hits += NeutralGroupHit.FromDefine(groups(lossElement.Key))
                 hits.Last.nHit = lossElement.Value
             Next
