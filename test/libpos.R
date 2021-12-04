7c26036afd86aa0d5ab26653eb2fbecad0a5b3e4
xieguigang <xie.guigang@gcmodeller.org>
2021-12-05 02:57:54 +0800

    fix of test

--- test/libpos.R
+++ test/libpos.R
@@ -2,8 +2,8 @@ require(PlantMAT);
 
 options(strict = FALSE);
 
-path = "D:\GCModeller\src\R-sharp\studio\RData\test\test_list.rda";
-# path = "D:\biodeep\flavonoid\Rscript\bundle\Flavonoid.rda";
+# path = "D:\GCModeller\src\R-sharp\studio\RData\test\test_list.rda";
+path = "E:\biodeep\biodeep_pipeline\biodeepNPSearch\data\Flavonoid.rda";
 libpos = PlantMAT::read.library(path, libtype = 1);
 
 print(as.data.frame(libpos));
