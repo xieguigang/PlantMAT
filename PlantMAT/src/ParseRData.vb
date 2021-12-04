5a506249b50eed8cb4b06c3f08a14c0356d0ac7c
xieguigang <xie.guigang@gcmodeller.org>
2021-12-05 03:06:56 +0800

    load biodeep library test success

--- PlantMAT/src/ParseRData.vb
+++ PlantMAT/src/ParseRData.vb
@@ -3,6 +3,7 @@ Imports SMRUCC.Rsharp.RDataSet.Convertor
 Imports SMRUCC.Rsharp.RDataSet.Struct
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports PlantMATlib = PlantMAT.Core.Models.Library
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Module ParseRData
 
@@ -19,9 +20,25 @@ Module ParseRData
     Private Iterator Function PopulateReference(ref As list) As IEnumerable(Of PlantMATlib)
         For Each compound As list In ref.data.Select(Function(obj) DirectCast(obj, list))
             Dim meta As list = compound("metainfo")
-            Dim id As String = meta.getByName("BioDeepID")
-            Dim name As String = meta.getByName("name")
+            Dim id As String = REnv.single(meta.getByName("BioDeepID"))
+            Dim name As String = REnv.single(meta.getByName("name"))
+            Dim SMILES As String = REnv.single(meta.getByName("SMILES"))
+            Dim formula As String = REnv.single(meta.getByName("formula"))
+            Dim exactmass As Double = CDbl(REnv.single(meta.getByName("exact_mass")))
+            Dim type As String = REnv.single(meta.getByName("class"))
 
+            Yield New PlantMATlib With {
+                .CommonName = name,
+                .Xref = id,
+                .Universal_SMILES = SMILES,
+                .ExactMass = exactmass,
+                .Formula = formula,
+                .[Date] = Now,
+                .Editor = "BioDeep",
+                .Type = type,
+                .[Class] = .Type,
+                .Genus = .Type
+            }
         Next
     End Function
 End Module
