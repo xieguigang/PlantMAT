825fc9f4bbdf3e1cb7cdb63e87f98622056cc408
guigang xie <gg.xie@bionovogene.com>
2021-12-07 11:15:42 +0800

    Update PlantMAT.R

--- PlantMAT/workflow/PlantMAT.R
+++ PlantMAT/workflow/PlantMAT.R
@@ -1,5 +1,3 @@
-imports "" from "";
-
 require(PlantMAT);
 
 print("Run PlantMAT search, please wait for a while...");
@@ -11,5 +9,11 @@ print("Run PlantMAT search, please wait for a while...");
         analysis: 1. 'args' element for the analysis arguments and,
         2. 'library' element for set the reference library data."]
 [@type "RData(args, library)"]
-const stream = ?"--stream" || stop("A configuration data file location string which is encoded in base64 must be provided!");
+const stream as string = ?"--stream" || stop("A configuration data file location string which is encoded in base64 must be provided!");
+const [args, library]  = base::readRData(base64_decode(stream));
 
+print("View of the PlantMAT analysis arguments:");
+str(args);
+
+print(`we have ${length(library)} reference data to run PlantMAT search:`);
+print(library, max.print = 10);
