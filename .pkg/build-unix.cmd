2707f2547975ea50352402787f7e337ca15448af
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-01 20:19:14 +0800

    config script for run msbuild

--- .pkg/build-unix.cmd
+++ .pkg/build-unix.cmd
@@ -5,7 +5,7 @@ SET R_HOME=%drive%\GCModeller\src\R-sharp\App\net6.0
 SET Rscript="%R_HOME%/Rscript.exe"
 SET REnv="%R_HOME%/R#.exe"
 
-%Rscript% --build /src ../ /save ../NRRD.zip --skip-src-build
-%REnv% --install.packages ../NRRD.zip
+%Rscript% --build /src ../ /save ../PlantMAT.zip --skip-src-build
+%REnv% --install.packages ../PlantMAT.zip
 
 pause
