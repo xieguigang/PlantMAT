72da8b74215f6c86e973d68834d303754a7d6862
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-08 17:31:38 +0800

    create model

---
+++ src/Models/NPClassName.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class NPClassName
+
+    Public Property name As String
+    Public Property note As String
+    Public Property subclass As Dictionary(Of String, NPClassName)
+
+    Public Overrides Function ToString() As String
+        Return $"[{name}] {note}"
+    End Function
+
+    Public Shared Function ParseJSON(str As String) As Dictionary(Of String, NPClassName)
+        Return str.LoadJSON(Of Dictionary(Of String, NPClassName))
+    End Function
+
+End Class
